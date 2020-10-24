50bd12232373162881124ca8b7cb6b9cbd021be5
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-10-24 17:52:24 +0200

    cs and phpstan fixes

--- Models/TicketMapper.php
+++ Models/TicketMapper.php
@@ -41,7 +41,7 @@ final class TicketMapper extends DataMapperAbstract
     /**
      * Has one relation.
      *
-     * @var array<string, array{mapper:string, self:string, by?:string, column?:string}>
+     * @var array<string, array{mapper:string, external:string, by?:string, column?:string, conditional?:bool}>
      * @since 1.0.0
      */
     protected static array $ownsOne = [
