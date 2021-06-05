84a345f0dae6cd8011d1e1239ea07151635d0e17
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-06-05 11:01:03 +0200

    bug fixes

---
+++ Application/Ticket/Timerecording/Routes.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+return [
+    '^/timerecording$' => [
+        0 => [
+            'dest'       => '\Modules\HumanResourceTimeRecording\Controller\TimerecordingController:viewDashboard',
+            'verb'       => 1,
+            'permission' => [
+                'module' => 'HumanResourceTimeRecording',
+                'type'   => 2,
+                'state'  => 1,
+            ],
+        ],
+    ],
+    '^.*/timerecording/dashboard.*$' => [
+        0 => [
+            'dest'       => '\Modules\HumanResourceTimeRecording\Controller\TimerecordingController:viewDashboard',
+            'verb'       => 1,
+            'permission' => [
+                'module' => 'HumanResourceTimeRecording',
+                'type'   => 2,
+                'state'  => 1,
+            ],
+        ],
+    ],
+];
