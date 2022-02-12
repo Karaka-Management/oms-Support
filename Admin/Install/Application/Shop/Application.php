e7a6596d67a507a8eed541b090721ad79b241e4f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-02-12 20:09:06 +0100

    fix font path

--- Admin/Install/Application/Shop/Application.php
+++ Admin/Install/Application/Shop/Application.php
@@ -355,8 +355,8 @@ final class Application
     private function initResponseHead(Head $head, HttpRequest $request, HttpResponse $response) : void
     {
         /* Load assets */
-        $head->addAsset(AssetType::CSS, '../Resources/fontawesome/css/font-awesome.min.css');
-        $head->addAsset(AssetType::CSS, '../Web/{APPNAME}/css/fonts.css');
+        $head->addAsset(AssetType::CSS, '../Resources/fonts/fontawesome/css/font-awesome.min.css');
+        $head->addAsset(AssetType::CSS, '../Resources/fonts/Roboto/roboto.css');
         $head->addAsset(AssetType::CSS, '../Web/{APPNAME}/css/shop.css');
 
         // Framework
